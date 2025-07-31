23662ccc42a083a5b216500664ac097a7d473310
zahirul <zahirulopel@gmail.com>
2025-07-31 10:10:22 +0600

    get user info from github api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,36 @@
+import { useEffect, useState } from "react";
+
+const url = `https://api.github.com/users`;
+
 function App() {
+  const [userData, setUserData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [searchUser, setSearchUser] = useState("octocat");
+
+  useEffect(() => {
+    async function getUser() {
+      try {
+        const username = searchUser;
+        const response = await fetch(`${url}/${username}`);
+        if (response.status >= 400) {
+          throw new Error("Error on getting user data");
+        }
+        const data = await response.json();
+        setUserData(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getUser();
+  }, []);
+
+  if (error) return <p>{error.message}</p>;
+
+  if (isLoading) return <p>Loading</p>;
+
   return <></>;
 }
 
