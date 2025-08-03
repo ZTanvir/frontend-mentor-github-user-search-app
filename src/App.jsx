d1e61f7d5ac1e107e158eca4abbb50bfdbaf0213
zahirul <zahirulopel@gmail.com>
2025-08-03 09:58:16 +0600

    add font space mono

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "./App.css";
 import UserCard from "./components/UserCard";
 import SearchBar from "./components/SearchBar";
 import Theme from "./components/Theme";
