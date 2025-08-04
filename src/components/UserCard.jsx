9acde796a0859fc5551479d7cda253d753b91eae
zahirul <zahirulopel@gmail.com>
2025-08-04 11:34:19 +0600

    design userCard for desktops

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -10,80 +10,92 @@ const UserCard = ({ userData }) => {
     .join(" ");
 
   return (
-    <section className="user-container">
-      <div className="user-profile">
-        <img className="desktop-avatar-img" src={userData.avatar_url} alt="" />
-        <div>
-          <h2 className="username">{userData.name}</h2>
-          <h3 className="user-id">@{userData.login}</h3>
+    <section
+      style={{ "--gutter": "1rem" }}
+      data-split="fraction:auto/1"
+      className="user-container"
+    >
+      <img className="desktop-avatar-img" src={userData.avatar_url} alt="" />
+      <div
+        data-stack
+        style={{ "--gutter": "1rem" }}
+        className="user-descriptions"
+      >
+        <div data-split="fraction:1/1" className="user-profile">
+          <div>
+            <h2 className="username">{userData.name}</h2>
+            <h3 className="user-id">@{userData.login}</h3>
+          </div>
+          <p>Joined {createdAt}</p>
         </div>
-        <p>{createdAt}</p>
-      </div>
-      <p className="user-bio">
-        {userData.bio === null ? "This profile has no bio" : userData.bio}
-      </p>
-      <div data-split="fraction:1/1/1" className="user-data">
-        <div data-stack className="repos">
-          <span>Repos</span>
-          <span>{userData.public_repos}</span>
+        <p className="user-bio">
+          {userData.bio === null ? "This profile has no bio" : userData.bio}
+        </p>
+        <div data-split="fraction:1/1/1" className="user-data">
+          <div data-stack className="repos">
+            <span>Repos</span>
+            <span>{userData.public_repos}</span>
+          </div>
+          <div data-stack className="followers">
+            <span>Followers</span>
+            <span>{userData.followers}</span>
+          </div>
+          <div data-stack className="following">
+            <span>Following</span>
+            <span>{userData.following}</span>
+          </div>
         </div>
-        <div data-stack className="followers">
-          <span>Followers</span>
-          <span>{userData.followers}</span>
-        </div>
-        <div data-stack className="following">
-          <span>Following</span>
-          <span>{userData.following}</span>
-        </div>
-      </div>
-      <div data-split="fraction:1/1" className="user-social">
-        <div data-stack style={{ "--gutter": "1rem" }}>
-          <a data-inline href="#" style={{ "--gutter": "1rem" }}>
-            <img src={locationImg} alt="location of the user" />
-            <span className={Boolean(userData.location) ? "found" : "notFound"}>
-              {Boolean(userData.location) ? userData.location : "Not available"}
-            </span>
-          </a>
-          <a data-inline href={userData.blog} style={{ "--gutter": "1rem" }}>
-            <img src={websiteImg} alt="blog of the user" />
-            <span className={Boolean(userData.blog) ? "found" : "notFound"}>
-              {Boolean(userData.blog) ? userData.blog : "Not available"}
-            </span>
-          </a>
-        </div>
-        <div data-stack style={{ "--gutter": "1rem" }}>
-          <a
-            data-inline
-            href={userData.twitter_username}
-            style={{ "--gutter": "1rem" }}
-          >
-            <img src={twitterImg} alt="twitter account of the user" />
-            <span
-              className={
-                !Boolean(userData.twitter_username) ? "found" : "notFound"
-              }
+        <div data-split="fraction:1/1" className="user-social">
+          <div data-stack style={{ "--gutter": "1rem" }}>
+            <a data-inline href="#" style={{ "--gutter": "1rem" }}>
+              <img src={locationImg} alt="location of the user" />
+              <span
+                className={Boolean(userData.location) ? "found" : "notFound"}
+              >
+                {Boolean(userData.location)
+                  ? userData.location
+                  : "Not available"}
+              </span>
+            </a>
+            <a data-inline href={userData.blog} style={{ "--gutter": "1rem" }}>
+              <img src={websiteImg} alt="blog of the user" />
+              <span className={Boolean(userData.blog) ? "found" : "notFound"}>
+                {Boolean(userData.blog) ? userData.blog : "Not available"}
+              </span>
+            </a>
+          </div>
+          <div data-stack style={{ "--gutter": "1rem" }}>
+            <a
+              data-inline
+              href={userData.twitter_username}
+              style={{ "--gutter": "1rem" }}
             >
-              {Boolean(userData.twitter_username)
-                ? userData.twitter_username
-                : "Not available"}
-            </span>
-          </a>
-          <a
-            data-inline
-            href={userData.organizations_url}
-            style={{ "--gutter": "1rem" }}
-          >
-            <img src={companyImg} alt="company website of the user" />
-            <span
-              className={
-                Boolean(userData.organizations_url) ? "found" : "notFound"
-              }
+              <img src={twitterImg} alt="twitter account of the user" />
+              <span
+                className={
+                  Boolean(userData.twitter_username) ? "found" : "notFound"
+                }
+              >
+                {Boolean(userData.twitter_username)
+                  ? userData.twitter_username
+                  : "Not available"}
+              </span>
+            </a>
+            <a
+              data-inline
+              href={userData.html_url}
+              style={{ "--gutter": "1rem" }}
             >
-              {Boolean(userData.organizations_url)
-                ? userData.organizations_url
-                : "Not available"}
-            </span>
-          </a>
+              <img src={companyImg} alt="company website of the user" />
+              <span
+                className={Boolean(userData.html_url) ? "found" : "notFound"}
+              >
+                {Boolean(userData.html_url)
+                  ? userData.html_url
+                  : "Not available"}
+              </span>
+            </a>
+          </div>
         </div>
       </div>
     </section>
